dbbb179bd25208d411467c2c3c8f461207cd1403
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-15 17:20:51 +0000

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* this function prints a specific text using printf
+* Return: 0 on success
+*/
+int main(void)
+{
+	#This will print the text between brackets
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
