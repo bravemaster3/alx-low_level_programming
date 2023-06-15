a5147a4a1b64043fdf71219c17717bb3850fda41
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-15 20:01:37 +0000

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*This uses a non-common fonction to write to the stderr
+*Return: 1 in this case
+*/
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(str, sizeof(char), strlen(str), stderr);
+	return (1);
+}
