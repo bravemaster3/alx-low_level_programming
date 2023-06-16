8bb4411900f916681622bfcd85e8f0a8f1b7db65
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-16 17:24:46 +0000

    Add 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+*main - Entry point
+*
+* testing number and printing specific output on conditions
+*
+*Return: 0 on success
+*/
+
+int main(void)
+{
+	int n, last;
+	char compl[50];
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	if (last > 5)
+		strcpy(compl, "and is greater than 5\n");
+	else if (last == 0)
+		strcpy(compl, "and is 0\n");
+	else
+		strcpy(compl, "and is less than 6 and not 0\n");
+	printf("Last digit of %d is %d and %s", n, last, compl);
+	return (0);
+}
