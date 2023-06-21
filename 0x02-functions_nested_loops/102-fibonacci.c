527c2dd7228d1986f13b17dc394ebb32021496f2
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-21 20:15:04 +0000

    Add 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point, print first 50 fibonacci
+*
+* Return: no return
+*/
+
+void main(void)
+{
+	int i, n1 = 1, n2 = 2, n3;
+
+	printf("%d, %d, ", n1, n2);
+	for (i = 1; i <= 50; i++)
+	{
+		n3 = n1 + n2;
+		if (i == 50)
+			printf("%d", n3);
+		else
+			printf("%d, ", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+	printf("\n");
+}
