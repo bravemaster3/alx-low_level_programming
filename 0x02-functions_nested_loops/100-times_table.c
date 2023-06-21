e4f058e918ce2a51cb1ca8f581c4cafe4f0ad8a5
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-21 19:40:28 +0000

    Add 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_times_table - prints the time table from 0 to n
+* @n: an integer which the times table will stop
+* Return: No return
+*/
+
+void print_times_table(int n)
+{
+	int i, j, p;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				p = i * j;
+				if (j > 0)
+				{
+					if (p < 10)
+						printf(" ");
+					if (p < 100)
+						printf(" ");
+					if (p < 1000)
+						printf(" ");
+				}
+				printf("%d", p);
+				if (j < n)
+					printf(",");
+			}
+			printf("\n");
+		}
+	}
+}
