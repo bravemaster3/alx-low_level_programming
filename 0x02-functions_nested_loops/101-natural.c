c6847cf479820215f7a859af4d08b1470d9b279d
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-21 20:00:09 +0000

    Add 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
