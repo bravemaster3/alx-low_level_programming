2ebddab1bd130c3a49b37fe996320f7c147ad582
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-21 21:01:14 +0000

    Add 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 	int i;
-	long long int n1 = 1, n2 = 2, n3;
+	long int n1 = 1, n2 = 2, n3;
 
-	printf("%lld, %lld, ", n1, n2);
+	printf("%uld, %uld, ", n1, n2);
 	for (i = 3; i <= 98; i++)
 	{
 		n3 = n1 + n2;
 		if (i == 98)
-			printf("%lld", n3);
+			printf("%uld", n3);
 		else
-			printf("%lld, ", n3);
+			printf("%uld, ", n3);
 		n1 = n2;
 		n2 = n3;
 	}
