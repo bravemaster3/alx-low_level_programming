638fbf7b91cbee888ffec33ce31f3356438a4510
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-22 20:07:28 +0000

    corrected Betty

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,17 +12,14 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size - i; j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(' ');
+			for (j = 1; j <= size - i; j++)
+				_putchar(' ');
+			for (k = 1; k <= i; k++)
+				_putchar('#');
+			_putchar('\n');
 		}
-		for (k = 1; k <= i; k++)
-                                _putchar('#');
-		_putchar('\n');
-
-	}
 	}
 	else
 		_putchar('\n');
