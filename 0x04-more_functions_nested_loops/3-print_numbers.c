f7708f2bab6032d9ee81bfdec6bfc8a4f337c6a2
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-22 17:02:28 +0000

    Add 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - this function prints all single digit numbers
+* it takes no parameters
+* Return: no return
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '0'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
