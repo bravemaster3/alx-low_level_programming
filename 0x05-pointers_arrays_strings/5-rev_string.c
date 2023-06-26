afac8fb276bb4b434d071ce563a24d669c9ec449
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-26 19:49:36 +0000

    Add 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse, no newline
+ * @s: the pointer variable to the string
+ * Return: No return
+ */
+void rev_string(char *s)
+{
+	int counter1 = 0, counter2 = 0;
+	char c;
+	char *s1 = s;
+
+	while (*s1 != '\0')
+	{
+		counter1++;
+		s1++;
+	}
+	counter1--;
+	while (counter1 >= 0 && counter2 < counter1)
+	{
+		c = *(s + counter2);
+		*(s + counter2) = *(s + counter1);
+		*(s + counter1) = c;
+		counter1--;
+		counter2++;
+	}
+}
