06a6e7192ef49831be6fb1f38b7ba0b3b6861c4e
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-29 22:12:25 +0000

    Correct 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,22 +9,22 @@
 
 char *cap_string(char *str)
 {
-    int i = 1;
+	int i = 1;
 
-    while (str[i] != '\0')
-    {
-        if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
-            str[i - 1] == '\n' || str[i - 1] == ',' ||
-            str[i - 1] == ';' || str[i - 1] == '.' ||
-            str[i - 1] == '!' || str[i - 1] == '?' ||
-            str[i - 1] == '\"' || str[i - 1] == '(' ||
-            str[i - 1] == ')' || str[i - 1] == '{' ||
-            str[i - 1] == '}')
-        {
-            if (str[i] >= 97 && str[i] <= 122)
-                str[i] = str[i] - 32;
-        }
-        i++;
-    }
-    return (str);
+	while (str[i] != '\0')
+	{
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
+			str[i - 1] == '\n' || str[i - 1] == ',' ||
+			str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' ||
+			str[i - 1] == '\"' || str[i - 1] == '(' ||
+			str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}')
+		{
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
 }
