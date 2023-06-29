3965a453181877855045d591b56c216af7a134e7
Koffi <noumonvikoffidodji@hotmail.com>
2023-06-29 18:12:46 +0000

    Add 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -22,3 +22,21 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the pointer to a string
+ * Return: the length of the string that s points to
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
