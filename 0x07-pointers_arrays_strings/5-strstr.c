2aa2265d5f0238f8f399060f9d6758771f00d520
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-03 18:45:55 +0000

    Add 5 to test

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strstr - locates substring needle in haystack
@@ -8,14 +7,10 @@
  * Return: the pointer to the located substring
  */
 
-int _strlen(char *s);
-
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j, counter;
 	char *s1 = '\0';
-
-	printf("LENGTH of needle: %i\n", _strlen(needle));
 
 	for (i = 0; *(haystack + i) != '\0'; i++)
 	{
@@ -27,35 +22,27 @@ char *_strstr(char *haystack, char *needle)
 						*(haystack + i + j) != '\0';
 				 j++)
 			{
-				printf("Current letters:%c:%c\n", *(haystack + i), *(needle + j));
 				if (*(haystack + i + j) == *(needle + j))
 				{
 					counter += 1;
-					printf("current counter: %d\n", counter);
 				}
 				else
 				{
 					counter = 0;
-					printf("Reinitializing counter\n");
 				}
 
 				if (counter == _strlen(needle))
 				{
-					printf("Before last increment\n");
 					s1 = haystack + i;
-					printf("last increment also worked\n");
 					goto end;
 				}
 				else
 					s1 = '\0';
 			}
 		}
-		printf("current S1: %p\n", s1);
-		printf("Current i is:%d\n", i);
 	}
 	goto end;
 end:
-	printf("We are at the end\n");
 	return (s1);
 }
 
