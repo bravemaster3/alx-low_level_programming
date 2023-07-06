3c95eb378c1740bb1a4792b6f9db7bed562e2571
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-06 21:00:53 +0000

    101-wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * pass_asterisk - fasts forward s2 when asterisk found, to move to next char
+ * @s2: pointer to second string
+ * Return: a pointer to s2 after *
+ */
 char *pass_asterisk(char *s2)
 {
 	if (*s2 == '*' && *s2 != '\0')
@@ -8,10 +13,15 @@ char *pass_asterisk(char *s2)
 	return (s2);
 }
 
+/**
+ * wildcmp_asterisk - compares s1 characters when s2 when s2 has *
+ * @s1: pointer to the first string
+ * @s2: pointer to second string
+ * Return: a pointer to the last s1 match?
+ */
 char *wildcmp_asterisk(char *s1, char *s2)
 {
 	if (*s1 != *s2 && *s1 != '\0')
-		/*Try something here, a condition for incrementing S1 to the end even when there is a match, to see if we find also the same characters....*/
 		return (wildcmp_asterisk(s1 + 1, s2));
 
 	return (s1);
@@ -21,13 +31,14 @@ char *wildcmp_asterisk(char *s1, char *s2)
  * wildcmp - compares two strings
  * @s1: pointer to the first string
  * @s2: pointer to second string
+ * Return: an integer
  */
 
 int wildcmp(char *s1, char *s2)
 {
 	int ret;
-	ret = 0;
 
+	ret = 0;
 	if (*s1 != *s2 && *s2 != '*')
 	{
 		ret = 0;
