f11be85d51adf9170741e9a0f049190d30bc2204
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-10 18:42:11 +0000

    Add 1 duplicate any string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen_recursion - returns the length of a function using recursion
+ * @s: pointer to the string
+ * Return: the length of the string as integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s == '\0')
+		return (0);
+	len = 1 + _strlen_recursion(s + 1);
+	return (len);
+}
+
+/**
+ * _strdup - duplicates a string
+ * @str: pointer to the string to duplicate
+ * Return: pointer to the copy of the string
+ */
+
+char *_strdup(char *str)
+{
+	char *str_cpy;
+	int len = _strlen_recursion(str), i;
+
+	if (str == NULL)
+		return (NULL);
+
+	str_cpy = malloc(sizeof(*str_cpy) * len);
+	for (i = 0; i < len; i++)
+		str_cpy[i] = str[i];
+
+	return (str_cpy);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,7 @@
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+int _strlen_recursion(char *s);
+char *_strdup(char *str);
 
 #endif
