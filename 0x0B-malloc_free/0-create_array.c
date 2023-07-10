436273eac3a6fccf3994b78e11105542a696bbf3
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-10 18:09:16 +0000

    Add 0, my first malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,14 +14,14 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
+
 	str = malloc(sizeof(*str) * size);
 
+	if (str == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
-	{
 		str[i] = c;
-	}
 	return (str);
 }
