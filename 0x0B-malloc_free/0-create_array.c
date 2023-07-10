c8138fa38190dbdb3f8b6ecf20b6915e1cbf90e9
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-10 18:03:29 +0000

    Add 0, my first malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of size size, initialized with char c
+ * @size: unsigned integer, size of the array
+ * @c: character to be used for initialization
+ * Return: a pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	str = malloc(sizeof(*str) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	free(str);
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+# Malloc and free:
+Memory allocation and delocation
+Always make sure to free any memory that was allocated to avoid memory leaks.
