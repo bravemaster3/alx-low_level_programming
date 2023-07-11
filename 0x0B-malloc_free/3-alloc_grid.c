090cbde63613760c5ca95b3ab0fb5b8a9fa67d9c
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-11 07:32:47 +0000

    Correct 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,13 @@ int **alloc_grid(int width, int height)
 	{
 		arr_2D[i] = malloc(sizeof(int) * width);
 		if (arr_2D[i] == NULL)
+		{
+			while (--i >= 0)
+			{
+				free(arr_2D[i]);
+			}
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
