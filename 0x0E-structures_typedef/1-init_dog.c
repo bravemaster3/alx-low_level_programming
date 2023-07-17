845cdd7389c9d6f911a379c5bf10e8c47f2b6585
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-17 17:01:54 +0000

    Add 1 initialization of a struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,13 @@
 #include "dog.h"
 
+/**
+ * init_dog - initialized the values of a struct
+ * @d: pointer to the struct
+ * @name: pointer to the name of the doc
+ * @age: age of the dog
+ * @owner: pointer to the owner's name
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
