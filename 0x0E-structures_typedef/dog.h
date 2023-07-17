87cced657d715026101ddb95c9a7b45acfbe3875
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-17 16:50:37 +0000

    Add 0 struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new data type to describe dogs
+ * @name: pointer to the dog name
+ * @age: dog's age
+ * @owner: pointer to the dog owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
