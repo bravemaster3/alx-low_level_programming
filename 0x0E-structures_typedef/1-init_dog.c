51dee0d88ee5df336af6560cdac786b7adb63065
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-17 17:22:08 +0000

    Edit to also treat null as string

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initialized the values of a struct
@@ -10,7 +11,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (name == NULL)
+		name = "";
+	if (owner == NULL)
+		owner = "";
 	d->name = name;
 	d->age = age;
-	d->owner = owner;
+	(*d).owner = owner; /*Just for fun, I made this one different*/
 }
