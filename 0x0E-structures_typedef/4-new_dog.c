a7707200b86306b0616e3c0e270e0511111c06ad
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-17 18:51:21 +0000

    Still having problem with NULL elements

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - creates a new dog
  * @name: pointer to dog name
@@ -10,15 +11,30 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog_i = malloc(sizeof(dog_t));
+	dog_t *dog_i;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog_i = malloc(sizeof(dog_t));
 	if (dog_i == NULL)
+		return (NULL);
+
+	dog_i->name = malloc(strlen(name) + 1);
+	if (dog_i->name == NULL)
 	{
 		free(dog_i);
 		return (NULL);
 	}
-	dog_i->name = name;
+	strcpy(dog_i->name, name);
+
+	dog_i->owner = malloc(strlen(owner) + 1);
+	if (dog_i->owner == NULL)
+	{
+		free(dog_i->name);
+		free(dog_i);
+		return (NULL);
+	}
+	strcpy(dog_i->owner, owner);
 	dog_i->age = age;
-	dog_i->owner = owner;
 	return (dog_i);
 }
