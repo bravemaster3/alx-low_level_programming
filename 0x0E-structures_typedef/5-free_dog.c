afcd19b92ecb16ed9c02bbab65962a9043a948ce
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-17 19:00:25 +0000

    Free all dogs 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog
+ * @d: pointer to the structure of the dog
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
