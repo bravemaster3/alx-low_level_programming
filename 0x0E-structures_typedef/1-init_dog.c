ff9dbe6ef9e1c5ae46cb6c56ba5f868293a7a883
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-17 17:24:14 +0000

    check for negative age

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,6 +15,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		name = "";
 	if (owner == NULL)
 		owner = "";
+	if (age < 0)
+		age = 0;
 	d->name = name;
 	d->age = age;
 	(*d).owner = owner; /*Just for fun, I made this one different*/
--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	struct dog my_dog;
 
-	init_dog(&my_dog, NULL, 3.5, "Bob");
+	init_dog(&my_dog, NULL, -1, "Bob");
 	printf("My name is %s, and I am %.1f :) - WoofOWNED BY %s\n", my_dog.name, my_dog.age, my_dog.owner);
 	return (0);
 }
