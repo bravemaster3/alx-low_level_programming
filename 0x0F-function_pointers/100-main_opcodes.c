dc257b785de8f5a9e826833a16dede429c51e00a
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-19 21:30:21 +0000

    Add 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: No return
+ */
+
+void main(int argc, char **argv)
+{
+	int i;
+	void (*main_p)(int, char **) = &main;
+	char *ptr = (char *)main_p;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = atoi(argv[1]) - 1; i >= 0; i--)
+	{
+		printf("%x", ptr[i]);
+		if (i == atoi(argv[1] - 1))
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
