cfff6341a167a9074ebaa706df5ef13ad5668a26
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-20 20:12:09 +0000

    Add 3, no nill yet and 2 IF exhausted

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints all given types and variadic arguments
+ * @format: format specifier(s)
+ */
+void print_all(const char *const format, ...)
+{
+	unsigned int i = 0, j;
+	char array[] = "cifs", str_print[3] = "%?";
+	va_list all;
+
+	va_start(all, format);
+	while (*(format + i) != '\n')
+	{
+		j = 0;
+		while (j <= 3)
+		{
+			if (*(format + i) == array[j])
+			{
+				str_print[1] = *(format + i);
+				switch (*(format + i))
+				{
+				case 'c':
+					printf(str_print, va_arg(all, int));
+					break;
+				case 'i':
+					printf(str_print, va_arg(all, int));
+					break;
+				case 'f':
+					printf(str_print, va_arg(all, double));
+					break;
+				case 's':
+					printf(str_print, va_arg(all, char *));
+					break;
+				}
+				if (i < 3)
+					printf(", ");
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(all);
+	printf("\n");
+}
