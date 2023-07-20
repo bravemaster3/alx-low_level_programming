900ce60be9c98937a4f34c19b6a1a25aae44e516
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-20 15:57:49 +0000

    Add 1 print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(nums, unsigned int));
+		printf("%d", va_arg(ap, unsigned int));
 		if (i == n - 1)
 			printf("\n");
 		else
