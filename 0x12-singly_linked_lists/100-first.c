3bb1d4e37bbedc5f788fdfc8f33670b9d4083eb8
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-27 20:56:59 +0000

    Add 100 before main constructor...

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
+/**
+ * before_main - function that runs before main
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
