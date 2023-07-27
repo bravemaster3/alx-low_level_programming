975612be1e9e39ad8b16f726f99ed666c35d1597
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-27 18:04:02 +0000

    Add 0-print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints a singly linked list
+ * @h: header of the list, i.e. pointer to the first element
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *temp = h;
+	int len = 0;
+
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+			printf("[%d] (nil)\n", temp->len);
+		else
+			printf("[%d] %s\n", temp->len, temp->str);
+
+		temp = temp->next;
+		len++;
+	}
+	return (len);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# Let's link up singly some list
+```
+I am a single node. Oh, me too! Alright. Let's link up to create some list.
+Oh, we need a head :)
+```
