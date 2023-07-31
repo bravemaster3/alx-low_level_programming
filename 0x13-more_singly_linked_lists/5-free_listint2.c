d4b33029b711d5be9f9fee5f8533d0cf79d5b0ba
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-31 18:08:16 +0000

    Add 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,8 +6,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *cpy = *head;
+	listint_t *temp, *cpy;
 
+	if (head == NULL)
+		return;
+	cpy = *head;
 	while (cpy != NULL)
 	{
 		temp = cpy->next;
