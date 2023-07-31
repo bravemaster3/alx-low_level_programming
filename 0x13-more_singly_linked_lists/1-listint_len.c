aeffb7ac4e9c0664cb2185433652ab915983037e
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-31 15:57:18 +0000

    Add 2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts the number of elements in a list
+ * @h: pointer to the first element of the list
+ * Return: the length of the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *ptr = h;
+	size_t len = 0;
+
+	if (ptr == NULL)
+		return (0);
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		len++;
+	}
+	return (len);
+}
