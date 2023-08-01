c42549cea0076cd355dd4e8bf73ba7f7ffc257da
Koffi <noumonvikoffidodji@hotmail.com>
2023-08-01 22:03:36 +0000

    Add 103 not tested

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * find_listint_loop - catches any loops in an int list
+ * @head: pointer to the first element of the list
+ * Return: pointer to the start of the loop, or NULL if no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	const listint_t *slow = head, *fast = head;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		/* printf("[%p] %d\n", (void *)(slow), slow->n); */
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (slow);
+		}
+	}
+	return (NULL);
+}
