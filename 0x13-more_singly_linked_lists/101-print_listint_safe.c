9155500fb91e3ca85dc0b255aff28863ba9bd7e7
Koffi <noumonvikoffidodji@hotmail.com>
2023-08-01 21:48:02 +0000

    Corrected 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,38 +3,51 @@
 #include <stdlib.h>
 
 /**
+ * list_cycle_detector - catches any loops in an int list
+ * @head: pointer to the first element of the list
+ * Return: 0 if no loop was found, 1 if a loop was found
+ */
+int list_cycle_detector(const listint_t *head)
+{
+	const listint_t *slow = head, *fast = head;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		/* printf("[%p] %d\n", (void *)(slow), slow->n); */
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
  * print_listint_safe - prints a singly linked list of integers
  * @head: pointer to the first element of the list
  * Return: the number of elements of the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow, *fast;
+	const listint_t *copy = head;
 	size_t len = 0;
+	int hasCycle = list_cycle_detector(head);
 
-	if (head == NULL)
-		return (0);
-	slow = head,
-	fast = head;
-
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	while (copy != NULL)
 	{
-		printf("[%p] %d\n", (void *)(slow), slow->n);
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		printf("[%p] %d\n", (void *)copy, copy->n);
+		len++;
+
+		if (hasCycle && copy->next >= copy)
 		{
-			printf("-> [%p] %d\n", (void *)(slow), slow->n);
-			exit(98);
+			printf("-> [%p] %d\n", (void *)copy->next, copy->next->n);
+			break;
 		}
-		len++;
+
+		copy = copy->next;
 	}
 
-	while (slow != NULL)
-	{
-		printf("[%p] %d\n", (void *)(slow), slow->n);
-		slow = slow->next;
-		len++;
-	}
-	return (len);
+	return len;
 }
