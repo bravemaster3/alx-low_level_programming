fd79bdc906889639db8dad9c1eac03959f5777ac
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-14 14:16:44 +0000

    corrected Betty 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,17 +7,17 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    dlistint_t *ptr = head;
-    unsigned int i = 0;
+	dlistint_t *ptr = head;
+	unsigned int i = 0;
 
-    if (head == NULL)
-        return (NULL);
-    while (ptr)
-    {
-        if (i == index)
-            return (ptr);
-        i++;
-        ptr = ptr->next;
-    }
-    return (NULL);
+	if (head == NULL)
+		return (NULL);
+	while (ptr)
+	{
+		if (i == index)
+			return (ptr);
+		i++;
+		ptr = ptr->next;
+	}
+	return (NULL);
 }
