ba170315e9f63ea6cca300cfbe878aa51e611b1e
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-14 15:55:06 +0000

    correct 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
