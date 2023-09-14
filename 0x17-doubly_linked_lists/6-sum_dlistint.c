85d472997bbd6e31575126aedd47b9756535df74
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-14 14:26:18 +0000

    Add 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sums all integers of a linked list
+ * @head: head of the list
+ * Return: the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *ptr = head;
+
+	if (head == NULL)
+		return 0;
+
+	while (ptr)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
+}
