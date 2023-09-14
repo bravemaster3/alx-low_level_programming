632853f71a29927f76ee67e55ea30405350a5888
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-14 15:45:41 +0000

    Add 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a node at a given index
+ * @h: pointer to the head pointer
+ * @idx: index where the node should be inserted
+ * @n: integer to be inserted
+ * Return: pointer to the inserted node or NULL if insertion failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t)), *ptr;
+	unsigned int i = 0;
+
+	if (new == NULL || h == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *h;
+		*h = new;
+		return (new);
+	}
+	ptr = *h;
+	while (ptr)
+	{
+		if (i == idx)
+		{
+			ptr = ptr->prev; /*move one pointer back*/
+			new->next = ptr->next;
+			new->prev = ptr;
+			ptr->next = new;
+
+			return (new);
+		}
+		i++;
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
