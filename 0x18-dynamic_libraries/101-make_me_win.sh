a1cc2dbe49e5548f581641fde47248ef1f6c26e3
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-09-18 20:34:52 +0000

    made the library link fetched on github

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ./libmyrand.so /tmp/
+wget https://github.com/bravemaster3/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmyrand.so -P /tmp/
 export LD_PRELOAD=/tmp/libmyrand.so
