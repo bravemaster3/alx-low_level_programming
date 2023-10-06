92bb1e6b9b8410fb6da92fd3eaf09417add34b70
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-06 00:19:44 +0000

    Add strduped value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,10 +15,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || ht->size == 0 || key == NULL || strcmp(key, "") == 0)
 		return (0);
 
+	new_item = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (new_item == NULL)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_item->key = (char *)key;
 	new_item->next = NULL;
-	new_item->value = strdup((char *)value);
+	if (value != NULL)
+		new_item->value = strdup((char *)value);
 
 	if (ht->array[index] == NULL)
 		ht->array[index] = new_item;
